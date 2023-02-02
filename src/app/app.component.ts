196249d9d4934898fb1bd808a2a0a01b7c5f3014
Gavin-1001 <gavinshelley10@gmail.com>
2023-02-02 16:23:26 +0000

    commit this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'teamapp';
+    newMemberName: string = "";
+
+    //declare an empty array of string to store in the name section
+    arrayOfMembers: string[] = [];
+
+    addMember() {
+        console.log("Clicked");
+        this.arrayOfMembers.push(this.newMemberName);
+        this.newMemberName = "";
+        console.log(this.arrayOfMembers);
+    }
+
+    onInput(member: string) {
+        this.newMemberName = member;
+        console.log(this.newMemberName);
+    }
 }
