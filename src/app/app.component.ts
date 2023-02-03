633c56b762d56a880676f88be4a94985572d586d
Gavin-1001 <gavinshelley10@gmail.com>
2023-02-03 17:10:39 +0000

    Added error handling to prevent user from adding an empty string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,19 @@ export class AppComponent {
     //declare an empty array of string to store in the name section
     arrayOfMembers: string[] = [];
 
+    errorMessage = "";
+
     addMember() {
+
+        if(!this.newMemberName){
+            this.errorMessage = "Name cannot be empty";
+            return; //the return prevents an empty string from being added to the list
+
+        }
+
         console.log("Clicked");
         this.arrayOfMembers.push(this.newMemberName);
-        this.newMemberName = "";
+        this.newMemberName = ""; //this is getting referred from the html file. So that when the name is submitted on the input, it will reinitialize the input box to an empty box(empty string)
         console.log(this.arrayOfMembers);
     }
 
