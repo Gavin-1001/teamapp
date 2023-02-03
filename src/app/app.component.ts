469dc04ff7816e0371266ee071c8e5fe6603b5a0
Gavin-1001 <gavinshelley10@gmail.com>
2023-02-03 20:59:55 +0000

    Added click event for the generate button, initial logic and vars for dividing the names into teams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,15 @@ export class AppComponent {
 
     errorMessage = "";
 
+    numberOfTeams : number | string = ""; //this can be a number of "4" or 4 and convert "4" to a number
+
     addMember() {
 
-        if(!this.newMemberName){
+        if (!this.newMemberName) {
             this.errorMessage = "Name cannot be empty";
             return; //the return prevents an empty string from being added to the list
-
         }
-
-        console.log("Clicked");
+        this.errorMessage = "";
         this.arrayOfMembers.push(this.newMemberName);
         this.newMemberName = ""; //this is getting referred from the html file. So that when the name is submitted on the input, it will reinitialize the input box to an empty box(empty string)
         console.log(this.arrayOfMembers);
@@ -30,5 +30,13 @@ export class AppComponent {
     onInput(member: string) {
         this.newMemberName = member;
         console.log(this.newMemberName);
+    }
+
+    onNumberOfTeamsInput(value: string) {
+        this.numberOfTeams = Number(value); //converts the string of the value to a number for the numberOfTeams
+    }
+
+    generateTeams(){
+
     }
 }
